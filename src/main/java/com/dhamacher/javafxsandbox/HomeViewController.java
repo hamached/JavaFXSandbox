f75e61216915a89db23fd45b6a1aa3fa80d6ac3f
D. Hamacher <34037429+hamached@users.noreply.github.com>
2018-03-10 22:05:33 -0700

    Additional Changes

--- src/main/java/com/dhamacher/javafxsandbox/HomeViewController.java
+++ src/main/java/com/dhamacher/javafxsandbox/HomeViewController.java
@@ -9,8 +9,11 @@ import com.dhamacher.javafxsandbox.model.Product;
 import com.dhamacher.javafxsandbox.service.ProductService;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -18,6 +21,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.SwipeEvent;
 
 /**
  * FXML Controller class
@@ -52,7 +57,9 @@ public class HomeViewController implements Initializable {
     private Label priceLabel;    
     
     private final ObservableList<Product> products = 
-            FXCollections.observableArrayList();           
+            FXCollections.observableArrayList(); 
+    
+    private ProductService productService;
 
     /**
      * Initializes the controller class.
@@ -61,9 +68,19 @@ public class HomeViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         try 
         {            
-            ProductService ps = new ProductService();
-            products.addAll(ps.GetAllProducts());
+            productService = new ProductService();
+            products.addAll(productService.GetAllProducts());
+            productListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Product>(){
+                public void changed(ObservableValue<? extends Product> observable, Product oldProduct, Product newProduct) {
+                    priceLabel.setText(String.valueOf(newProduct.getPrice()));
+                    colorLabel.setText(newProduct.getColor());
+                    categoryLabel.setText(newProduct.getCategory());
+               }                
+            });
+            
+            
             productListView.setItems(products);
+            
         }
         catch(Exception e)
         {
@@ -74,7 +91,31 @@ public class HomeViewController implements Initializable {
             alert.showAndWait();
         }
     }  
-    
-    
-    
+
+    @FXML
+    private void CategoryClickedEvent(MouseEvent event) {
+        EventType<?> type = event.getEventType();
+        String typeName = type.getName();    
+        try {
+            Button btn = (Button)event.getSource();
+            String btnText = btn.getText();
+            products.clear();
+            products.addAll(productService.GetAllProductsByCategory(btnText));
+            productListView.refresh();            
+        }
+        catch(Exception e)
+        {
+            MessageDialog.MessagePopup("ERROR", e.getMessage());            
+        }       
+    }  
+
+    @FXML
+    private void GetSelectedItem(MouseEvent event) {
+        
+    }
+
+    @FXML
+    private void ScrollDown(SwipeEvent event) {
+               
+    }
 }
